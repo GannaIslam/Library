34dd2414f3cffbbacca70b144a70f88bf74d5a0e
Ganna <gannaislam45@gmail.com>
2026-09-10 05:39:50 +0300

    edit

--- src/app/collections/collections.ts
+++ src/app/collections/collections.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-
-interface Archivist {
-  name: string;
-  role: string;
-  initials: string;
-}
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [RouterLink],
   templateUrl: './collections.html',
   styleUrls: ['./collections.css']
 })
 export class CollectionsComponent {
-  archivists: Archivist[] = [
-    { name: 'Elias Thorne', role: 'Head Conservator', initials: 'ET' },
-    { name: 'Lady Genevieve', role: 'Master of the Arcane Stacks', initials: 'LG' },
-    { name: 'Arthur Pendelton', role: 'Chief Cartographer', initials: 'AP' }
-  ];
 }
